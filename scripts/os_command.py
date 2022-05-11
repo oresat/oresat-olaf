81cc0808857b23c5a3fdcc2875deb94d882369b6
ryanpdx <rmedick@pdx.edu>
2022-05-11 13:18:14 -0700

    add main function to all scripts

--- scripts/os_command.py
+++ scripts/os_command.py
@@ -11,24 +11,30 @@ import canopen
 EDS_FILE = dirname(abspath(__file__)) + '/../oresat_app.eds'
 OS_COMMAND_INDEX = 0x1023
 
-parser = ArgumentParser(description='Send bash command over CAN bus')
-parser.add_argument('bus', help='CAN bus to use')
-parser.add_argument('node', help='device node name in hex')
-parser.add_argument('command', nargs='?', help='bash command to run, must in \
-inside of \'\'')
-args = parser.parse_args()
 
-network = canopen.Network()
-node = canopen.RemoteNode(int(args.node, 16), EDS_FILE)
-network.add_node(node)
-network.connect(bustype='socketcan', channel=args.bus)
+def main():
+    parser = ArgumentParser(description='Send bash command over CAN bus')
+    parser.add_argument('bus', help='CAN bus to use')
+    parser.add_argument('node', help='device node name in hex')
+    parser.add_argument('command', nargs='?', help='bash command to run, must in \
+    inside of \'\'')
+    args = parser.parse_args()
 
-node.sdo[OS_COMMAND_INDEX][1].raw = args.command.encode('utf-8')
+    network = canopen.Network()
+    node = canopen.RemoteNode(int(args.node, 16), EDS_FILE)
+    network.add_node(node)
+    network.connect(bustype='socketcan', channel=args.bus)
 
-while node.sdo[OS_COMMAND_INDEX][2].phys == 0xFF:
-    sleep(0.1)
+    node.sdo[OS_COMMAND_INDEX][1].raw = args.command.encode('utf-8')
 
-print('status: ' + hex(node.sdo[OS_COMMAND_INDEX][2].phys))
-print('reply:\n\n' + node.sdo[OS_COMMAND_INDEX][3].raw.decode('utf-8'))
+    while node.sdo[OS_COMMAND_INDEX][2].phys == 0xFF:
+        sleep(0.1)
 
-network.disconnect()
+    print('status: ' + hex(node.sdo[OS_COMMAND_INDEX][2].phys))
+    print('reply:\n\n' + node.sdo[OS_COMMAND_INDEX][3].raw.decode('utf-8'))
+
+    network.disconnect()
+
+
+if __name__ == '__main__':
+    main()
