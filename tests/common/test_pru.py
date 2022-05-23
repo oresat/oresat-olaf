2556dd4de4aba93a928b8c9dc55ea6f7820c44a4
ryanpdx <rmedick@pdx.edu>
2022-05-22 19:07:29 -0700

    add check for PRUs for PRU unittests

--- tests/common/test_pru.py
+++ tests/common/test_pru.py
@@ -1,9 +1,15 @@
 import unittest
+from os.path import isdir
 
 from olaf.common.pru import PRU, PRUState, PRUError
 
 
+PRU_EXIST = isdir('/dev/remoteproc/pruss-core0') and isdir('/dev/remoteproc/pruss-core1')
+
+
 class TestPRU(unittest.TestCase):
+
+    @unittest.skipUnless(PRU_EXIST, 'requires PRU hardware')
     def test_pru(self):
         PRU(0, 'am335x-pru0-fw')
         PRU(1, '/lib/firmware/am335x-pru1-fw')
@@ -21,6 +27,7 @@ class TestPRU(unittest.TestCase):
         with self.assertRaises(PRUError):
             PRU(-1, 'am335x-pru1-fw')
 
+    @unittest.skipUnless(PRU_EXIST, 'requires PRU hardware')
     def test_pru_control(self):
         pru0 = PRU(0, 'am335x-pru0-fw')
 
