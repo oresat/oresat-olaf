495096d0b916fd6a16b2251f96d2c75b4ef6fbe9
ryanpdx <rmedick@protonmail.com>
2023-04-11 20:42:56 -0700

    rework of sdo transfer script

--- olaf/scripts/sdo_transfer.py
+++ olaf/scripts/sdo_transfer.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-'''SDO transfer script
+'''
+SDO transfer script
 
 This scipt act as CANopen master node, allowing it to read and write other
 node's Object Dictionaries.
 '''
 
 import sys
-from argparse import ArgumentParser
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from struct import pack, unpack
 from enum import Enum, auto
 
@@ -15,49 +16,88 @@ import canopen
 
 class CANopenTypes(Enum):
     '''All valid canopen types supported'''
-    b = auto()
-    i8 = auto()
-    u8 = auto()
-    i16 = auto()
-    u16 = auto()
-    i32 = auto()
-    u32 = auto()
-    i64 = auto()
-    u64 = auto()
-    f32 = auto()
-    f64 = auto()
-    s = auto()
-    d = auto()  # DOMAIN type
+
+    BOOLEAN = auto()
+    INTEGER8 = auto()
+    UNSIGNED8 = auto()
+    INTEGER16 = auto()
+    UNSIGNED16 = auto()
+    INTEGER32 = auto()
+    UNSIGNED32 = auto()
+    INTEGER64 = auto()
+    UNSIGNED64 = auto()
+    REAL32 = auto()
+    REAL64 = auto()
+    VISABLE_STRING = auto()
+    UNICODE_STRING = auto()
+    OCTECT_STRING = auto()
+    DOMAIN = auto()
 
 
-DECODE_KEYS = {
-    CANopenTypes.b: '?',
-    CANopenTypes.i8: 'b',
-    CANopenTypes.u8: 'B',
-    CANopenTypes.i16: 'h',
-    CANopenTypes.u16: 'H',
-    CANopenTypes.i32: 'i',
-    CANopenTypes.u32: 'I',
-    CANopenTypes.i64: 'q',
-    CANopenTypes.f32: 'f',
-    CANopenTypes.f64: 'd',
+NICE_NAMES = {
+    CANopenTypes.BOOLEAN: ['b', 'bool', 'boolean'],
+    CANopenTypes.INTEGER8: ['i8', 'int8', 'integer8'],
+    CANopenTypes.UNSIGNED8: ['u8', 'uint8', 'unsiged8'],
+    CANopenTypes.INTEGER16: ['i16', 'int16', 'integer16'],
+    CANopenTypes.UNSIGNED16: ['u16', 'uint16', 'unsiged16'],
+    CANopenTypes.INTEGER32: ['i32', 'int32', 'integer32'],
+    CANopenTypes.UNSIGNED32: ['u32', 'uint32', 'unsiged32'],
+    CANopenTypes.INTEGER64: ['i64', 'int64', 'integer64'],
+    CANopenTypes.UNSIGNED64: ['u64', 'uint64', 'unsiged64'],
+    CANopenTypes.REAL32: ['f32', 'float32', 'r32', 'real32'],
+    CANopenTypes.REAL64: ['f64', 'float64', 'r64', 'real64'],
+    CANopenTypes.VISABLE_STRING: ['s', 'string', 'v', 'visable_string'],
+    CANopenTypes.UNICODE_STRING: ['u', 'unicode_string'],
+    CANopenTypes.OCTECT_STRING: ['o', 'octets', 'octet_string'],
+    CANopenTypes.DOMAIN: ['d', 'domain'],
 }
 
 
+DECODE_KEYS = {
+    CANopenTypes.BOOLEAN: '?',
+    CANopenTypes.INTEGER8: 'b',
+    CANopenTypes.INTEGER16: 'h',
+    CANopenTypes.INTEGER32: 'i',
+    CANopenTypes.INTEGER64: 'q',
+    CANopenTypes.UNSIGNED8: 'B',
+    CANopenTypes.UNSIGNED16: 'H',
+    CANopenTypes.UNSIGNED32: 'I',
+    CANopenTypes.UNSIGNED64: 'Q',
+    CANopenTypes.REAL32: 'f',
+    CANopenTypes.REAL64: 'd',
+}
+
+
+EPILOG = 'DATA_TYPE options are (case insensitive): \n  ' \
+    + '\n  '.join([', '.join(NICE_NAMES[i]) for i in NICE_NAMES])
+
+
 def main():
-    parser = ArgumentParser(description='Read or write value to a node\'s object \
-    dictionary')
-    parser.add_argument('bus', help='CAN bus to use')
-    parser.add_argument('node', help='device node name in hex')
-    parser.add_argument('mode', help='read or write')
-    parser.add_argument('index', help='Object Dictionary index in hex')
-    parser.add_argument('subindex', help='Object Dictionary subindex in hex')
-    parser.add_argument('type', help='data type')
-    parser.add_argument('value', nargs='?', default=0, help='data to write')
+    parser = ArgumentParser(
+        description='read or write value to a node\'s object dictionary',
+        formatter_class=RawDescriptionHelpFormatter,
+        epilog=EPILOG
+    )
+    parser.add_argument('bus', metavar='BUS', help='CAN bus to use (e.g., can0, vcan0)')
+    parser.add_argument('node', metavar='NODE', help='device node id in hex')
+    parser.add_argument('mode', metavar='MODE', help='r[ead] or w[rite] (e.g. r, read, w, write)')
+    parser.add_argument('index', metavar='INDEX',
+                        help='object dictionary index in hex (e.g. 0x7000)')
+    parser.add_argument('subindex', metavar='SUBINDEX',
+                        help='object dictionary subindex in hex (e.g. 0x10)')
+    parser.add_argument('data_type', metavar='DATA_TYPE', help='the data type, see list below')
+    parser.add_argument('value', metavar='VALUE', nargs='?', default=0,
+                        help='data to write or for only string/domain data types a path to a file '
+                             '(e.g. file:data.txt)')
     args = parser.parse_args()
 
-    # make sure valid type
-    co_type = CANopenTypes[args.type]
+    for key in NICE_NAMES:
+        if args.data_type.lower() in NICE_NAMES[key]:
+            co_type = key
+            break
+    else:
+        print(f'invalid data type "{args.data_type}"')
+        sys.exit(1)
 
     # convert hex str to int
     index = int(args.index, 16)
@@ -72,84 +112,44 @@ def main():
     if args.mode == 'r' or args.mode == 'read':
         try:
             raw_data = node.sdo.upload(index, subindex)
-        except Exception as exc:
-            print(exc)
             network.disconnect()
-            return
-        network.disconnect()
+        except Exception as e:
+            print(e)
+            network.disconnect()
+            sys.exit(1)
 
-        if co_type == CANopenTypes.b:
-            data = unpack('?', raw_data)
-        elif co_type == CANopenTypes.i8:
-            data = unpack('b', raw_data)
-        elif co_type == CANopenTypes.u8:
-            data = unpack('B', raw_data)
-        elif co_type == CANopenTypes.i16:
-            data = unpack('h', raw_data)
-        elif co_type == CANopenTypes.u16:
-            data = unpack('H', raw_data)
-        elif co_type == CANopenTypes.i32:
-            data = unpack('i', raw_data)
-        elif co_type == CANopenTypes.u32:
-            data = unpack('I', raw_data)
-        elif co_type == CANopenTypes.i64:
-            data = unpack('q', raw_data)
-        elif co_type == CANopenTypes.u64:
-            data = unpack('Q', raw_data)
-        elif co_type == CANopenTypes.f32:
-            data = unpack('f', raw_data)
-        elif co_type == CANopenTypes.f64:
-            data = unpack('d', raw_data)
-        elif co_type == CANopenTypes.s:
+        if co_type in DECODE_KEYS.keys():
+            data = unpack(DECODE_KEYS[co_type], raw_data)[0]
+        elif co_type == CANopenTypes.VISABLE_STRING:
             data = raw_data.decode('utf-8')
-            print(data)
-            sys.exit(0)
-        elif co_type == CANopenTypes.d:
-            print(raw_data)
-            sys.exit(0)
-        else:
-            print('invalid data type')
-            sys.exit(0)
+        elif co_type in [CANopenTypes.UNICODE_STRING, CANopenTypes.OCTECT_STRING,
+                         CANopenTypes.DOMAIN]:
+            data = raw_data
 
-        print(data[0])
+        print(data)
     elif args.mode == 'w' or args.mode == 'write':
-        if co_type == CANopenTypes.b:
-            raw_data = pack('?', int(args.value))
-        elif co_type == CANopenTypes.i8:
-            raw_data = pack('b', int(args.value))
-        elif co_type == CANopenTypes.u8:
-            raw_data = pack('B', int(args.value))
-        elif co_type == CANopenTypes.i16:
-            raw_data = pack('h', int(args.value))
-        elif co_type == CANopenTypes.u16:
-            raw_data = pack('H', int(args.value))
-        elif co_type == CANopenTypes.i32:
-            raw_data = pack('i', int(args.value))
-        elif co_type == CANopenTypes.u32:
-            raw_data = pack('I', int(args.value))
-        elif co_type == CANopenTypes.i64:
-            raw_data = pack('q', int(args.value))
-        elif co_type == CANopenTypes.u64:
-            raw_data = pack('Q', int(args.value))
-        elif co_type == CANopenTypes.f32:
-            raw_data = pack('f', float(args.value))
-        elif co_type == CANopenTypes.f64:
-            raw_data = pack('d', float(args.value))
-        elif co_type == CANopenTypes.s:
-            raw_data = args.value.encode('utf-8')
-        elif co_type == CANopenTypes.d:
-            raw_data = args.value
-        else:
-            print('invalid data type')
+        if co_type in [CANopenTypes.REAL32, CANopenTypes.REAL64]:
+            raw_data = pack(DECODE_KEYS[co_type], float(args.value))
+        elif co_type in DECODE_KEYS.keys():
+            raw_data = pack(DECODE_KEYS[co_type], int(args.value))
+        elif co_type in [CANopenTypes.VISABLE_STRING, CANopenTypes.UNICODE_STRING,
+                         CANopenTypes.OCTECT_STRING, CANopenTypes.DOMAIN]:
+            if args.value.startswith('file:'):
+                with open(args.value[5:], 'rb') as f:
+                    raw_data = f.read()
+            else:
+                raw_data = args.value
 
         try:
             node.sdo.download(index, subindex, raw_data)
-        except Exception as exc:
-            print(exc)
-        network.disconnect()
+            network.disconnect()
+        except Exception as e:
+            print(e)
+            network.disconnect()
+            sys.exit(1)
     else:
-        print('Invalid mode')
-        print('Must be read or write')
+        print('invalid mode\nmust be "r", "read", "w", or "write"')
+        sys.exit(1)
 
 
 if __name__ == '__main__':
