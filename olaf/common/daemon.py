00b56a5c6d6350dd024a86ae106aaf559f4e4364
ryanpdx <rmedick@protonmail.com>
2023-05-18 16:25:28 -0700

    add daemon class for controlling external daemon thru systemd

---
+++ olaf/common/daemon.py
@@ -0,0 +1,55 @@
+from enum import Enum
+
+from pydbus import SystemBus
+
+
+class DaemonState(Enum):
+    '''Systemd unit states.'''
+
+    ACTIVE = 0
+    RELOADING = 1
+    INACTIVE = 2
+    FAILED = 3
+    ACTIVATING = 4
+    DEACTIVATING = 5
+
+
+class Daemon:
+    '''Very small wrapper ontop of pydbus to abstract away D-Bus to control systemd daemons.'''
+
+    # only need on instance of these for all instances of Daemon
+    _bus = SystemBus()
+    _systemd = _bus.get('.systemd1')
+
+    def __init__(self, name: str):
+        '''
+        Parameters
+        ----------
+        name: str
+            The daemon's systemd service file name; i.e.: ``'mydaemon.service'``.
+        '''
+
+        self._name = name
+        unit_path = self._systemd.GetUnit(self._name)
+        self._unit = self._bus.get('.systemd1', unit_path)
+
+    def start(self):
+        '''Start the daemon.'''
+
+        self._unit.Start(self._name, 'fail')
+
+    def stop(self):
+        '''Stop the daemon.'''
+
+        self._unit.Stop(self._name, 'fail')
+
+    def restart(self):
+        '''Restart the daemon.'''
+
+        self._unit.Restar(self._name, 'fail')
+
+    @property
+    def status(self) -> DaemonState:
+        '''DaemonState: The state of the daemon.'''
+
+        return DaemonState[self._unit.ActiveState.upper()]
