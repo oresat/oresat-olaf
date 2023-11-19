f5f7811420a05fcfb88efa7b1f1829dde7db90fa
Kyle Klein <132229861+kk60503@users.noreply.github.com>
2023-11-19 15:40:19 -0800

    Add files via upload

--- tests/bpe/bin_check.py
+++ tests/bpe/bin_check.py
@@ -1,32 +1,35 @@
-# binary compare between in/out files
-
-import sys
+import argparse
 
 
-def bin_check(file1, file2):
-    with open(file1,'rb') as f:
+def bin_check(file1: str, file2: str):
+    """binary compare between in/out files"""
+    with open(file1, "rb") as f:
         data_in = f.read()
 
-    with open(file2, 'rb') as f:
+    with open(file2, "rb") as f:
         data_out = f.read()
 
     count = 0
 
     if len(data_in) != len(data_out):
-        print('In file and out file are not the same size!')
+        print("In file and out file are not the same size!")
 
     else:
         for dataI, dataO in zip(data_in, data_out):
             if dataI != dataO:
                 count += 1
 
-    print("Data In Size; Data Out Size; Total of Nonidentical Data Instances:")
+    print("\nData In Size; Data Out Size; Total of Nonidentical Data Instances:")
     print(len(data_in))
     print(len(data_out))
     print(count)
 
-# if calling from command line: python bin_check.py <file1> <file2>
-if __name__ == '__main__':
-    file1 = sys.argv[1]
-    file2 = sys.argv[2]
-    bin_check(file1, file2)   
+
+if __name__ == "__main__":
+    """if calling from command line: python bin_check.py <file1> <file2>"""
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file1")
+    parser.add_argument("file2")
+    args = parser.parse_args()
+    bin_check(args.file1, args.file2)
--- tests/bpe/cfc.py
+++ tests/bpe/cfc.py
@@ -1,32 +1,37 @@
-# simple program to convert raw cfc images to viewable file types
+""" simple program to convert raw cfc images to viewable file types"""
 
 import os
 import sys
-import numpy as np
+
 import cv2
+import numpy as np
 
 
 def cfc(infile, columns, rows):
-    path = os.path.split(infile)                                     
-    appnd = path[1]                                                         
-    newpath = os.path.split(path[0])                                        
-    fout = newpath[0] + '/converted/' + appnd 
-    outfile = fout + '.png'
-    
-    with open(infile, 'rb') as f:
+    """main cfc function"""
+
+    path = os.path.split(infile)
+    appnd = path[1]
+    newpath = os.path.split(path[0])
+    fout = newpath[0] + "/converted/" + appnd
+    outfile = fout + ".png"
+
+    with open(infile, "rb") as f:
         raw = f.read()
-    # reshape() parameters are for end image dimensions
     data = np.frombuffer(raw, dtype=np.uint16).reshape(columns, rows)
-    # override with a copy that is mutable
-    data = np.copy(data)  
-    # manipulate image for displaying
-    data //= 64  # scale 14-bits to 8-bits
+    """ reshape() parameters are for end image dimensions"""
+    data = np.copy(data)
+    """ override with a copy that is mutable"""
+    data //= 64
+    """manipulate image for displaying - scale 14-bits to 8-bits"""
     data = data.astype(np.uint8)
-    data = np.invert(data)  # invert black/white values
+    data = np.invert(data)
+    """ invert black/white values"""
     cv2.imwrite(outfile, data)
 
-# if calling from command line: python cfc.py <infile> <columns> <rows>
+
 if __name__ == "__main__":
+    """if calling from command line: python cfc.py <infile> <columns> <rows>"""
     infile = sys.argv[1]
     columns = sys.argv[2]
     rows = sys.argv[3]
